d2a71785913be8cd5ee601cde31722c8c60dedfe
fireballpoint1 <fireball4556@gmail.com>
2020-05-20 16:37:17 +0530

    made output directory, need to run input files on ada

--- mayank/cp2k_energy_calculations/automate_cp2k_calculation.sh
+++ mayank/cp2k_energy_calculations/automate_cp2k_calculation.sh
@@ -1,14 +1,18 @@
 START=1
 END=201
 EXT=".in"
-DIR="."
+OUTEXT=".out"
+DIR="./"
+OUTDIR="output/"
 FILE="input"
+OUTFILE="output"
 for i in $(seq $START 1 $END)
 do
 	INPUT = $DIR$FILE$i$EXT
+	OUTPUT = $OUTDIR$OUTFILE$i$OUTEXT
 	if test -f $INPUT;then
 		echo "Using $INPUT"
-		~/cp2k-7.1-Linux-x86_64.ssmp $INPUT
+		~/cp2k-7.1-Linux-x86_64.ssmp -i $INPUT -o $OUTPUT
 	else
 		echo "$INPUT doesn't exists"
 	fi
